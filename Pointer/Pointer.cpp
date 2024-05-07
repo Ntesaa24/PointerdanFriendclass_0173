6695742569dea8a1c8af5c573d37bd98c4a8ddc3
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-07 10:41:47 +0700

    object mahasiswa

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 using namespace std; 
+
+class mahasiswa {
+public:
+	int nim; 
+	void showNim() {
+		cout << "No Induk = " << nim << endl; 
+	}
+};
+
+int main() {
+
+	mahasiswa mhs{ 1 }; // object mhs
+	mhs.showNim(); //Member Access Operator 
+
+	mahasiswa& refMhs = mhs; //Pointer Reference refMhs
+	refMhs.nim = 2; //Member access Operator 
+	mhs.showNim(); 
+
+	mahasiswa* pMhs = &mhs; //pointer Deference pMhs 
+	pMhs->nim = 3;  // Arraw Operator 
+	pMhs->showNim(); 
+	return 0; 
+
+}
